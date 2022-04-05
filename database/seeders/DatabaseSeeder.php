faeb2d4111621dfa705f05b1f1be97445b11fc30
Tiago Tavares <tiago@cyber-duck.co.uk>
2022-04-05 23:01:12 +0100

    Run Laravel breeze installation + Seed for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Sales Agent',
+            'email' => 'sales@coffee.shop',
+        ]);
     }
 }
--- docs/04-local-data.md
+++ docs/04-local-data.md
@@ -4,6 +4,6 @@ Feel free to document local data you add.
 
 We took the liberty of creating an initial Seeder with:
 1. User for you to login:
-   1. email: sales@coffee-shop.example
+   1. email: sales@coffee.shop
    2. password: password
 2. 
